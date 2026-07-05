e1a0c5503da09a97ab28bf04a385d003723f9969
Riyas <riyaszingbizz@gmail.com>
2026-07-05 21:11:42 +0530

    .

--- android/app/src/main/kotlin/com/example/music_room/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.music_room
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test.dart
+++
@@ -1,1 +0,0 @@
-import 'package:file_picker/file_picker.dart'; void main() { print(FilePicker.platform); }
