c68aada3cd0c917d3b8efa7fa4ee685f37b65f91
Riyas <riyaszingbizz@gmail.com>
2026-07-06 22:07:07 +0530

    logo changed, build optimised, librarry feature added, local storage added, background play optimised

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2424320275" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1254140176" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
